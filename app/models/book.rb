195abe1526da87b85d625f397266aaa67ad96b52
sandonemaki <kamuramaki@gmail.com>
2022-05-01 07:29:20 +0000

    book.rbのuniqunessを削除

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-  validates :book_title, presence: {message: "本のタイトルを入力してください"}, uniqueness: { scope: :book }
+  validates :book_title, presence: {message: "本のタイトルを入力してください"} #uniqueness: { scope: :book }
   validates :author, presence: {message: "著者を入力してください"}
 end
